114b59e03cae4effe41738290b3d2dc4a92cafea
stevefaeembra <stevendkay@gmail.com>
2023-02-09 21:15:15 +0000

    cope with open board, style fixes

--- src/DiceBoard.tsx
+++ src/DiceBoard.tsx
@@ -6,13 +6,13 @@ type Props = {};
 
 export default function DiceBoard({}: Props) {
   return (
-    <div className="mx-auto w-3/4 grid grid-cols-6">
+    <div className="mx-auto w-3/4 grid gap-0 grid-cols-6">
       <RollButton rollsLeft={3} />
-      <DiceCard roll={2} hold={false} />
-      <DiceCard roll={1} hold={true} />
-      <DiceCard roll={4} hold={false} />
-      <DiceCard roll={3} hold={false} />
-      <DiceCard roll={6} hold={false} />
+      <DiceCard roll={0} hold={false} />
+      <DiceCard roll={0} hold={false} />
+      <DiceCard roll={0} hold={false} />
+      <DiceCard roll={0} hold={false} />
+      <DiceCard roll={0} hold={false} />
     </div>
   );
 }
--- src/DiceCard.tsx
+++ src/DiceCard.tsx
@@ -7,17 +7,17 @@ type Props = {
 
 export default function DiceCard({ roll, hold }: Props) {
   const diceFileName = `/img/${roll}${hold ? "h" : ""}.png`;
-  const holdFileName = `img/${hold ? "hold1.png" : "hold0.png"}`;
+  const holdFileName = `/img/${hold && roll > 0 ? "hold1.png" : roll === 0 ? "0.png" : "hold0.png"}`;
   return (
     <div className="grid grid-rows-2">
       <div>
         <figure>
-          <img className="object-contain shadow-2xl" src={diceFileName} alt={roll.toString()} />
+          <img className="object-contain" src={diceFileName} alt={roll.toString()} />
         </figure>
       </div>
       <div>
         <figure>
-          <img className="object-contain shadow-2xl" src={holdFileName}></img>
+          <img className="object-contain" src={holdFileName}></img>
         </figure>
       </div>
     </div>
