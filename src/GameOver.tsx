646f7bccf23d82e475d5ddb46b45a0ef5d37c4c5
stevefaeembra <stevendkay@gmail.com>
2023-02-15 20:08:36 +0000

    implement high scores in local storage

--- src/GameOver.tsx
+++ src/GameOver.tsx
@@ -1,22 +1,40 @@
 import React from "react";
 import { GameState } from "./Types";
+import { useLocalStorage } from "usehooks-ts";
 
 type Props = {
   game: GameState;
 };
 
 export default function GameOver({ game }: Props) {
+  const [highScore, setHighScore] = useLocalStorage("dicegame.highscore", 0);
+
+  const updateHighScore = (total: number) => {
+    if (total > highScore) {
+      console.log("New high score", total);
+      setHighScore(total);
+    }
+  };
+
   console.log("Showing game over modal");
   const scores = game.scores.map((cat) => (cat.disabled ? cat.score : 0));
   const total = scores.reduce((a, b) => a + b, 0);
+
   return (
     <div className="grid">
       <div className="row">
         <h1 className="text-secondary">Game Over!</h1>
         <h1 className="text-secondary">You scored {total}</h1>
+        <h1 className="text-secondary">High Score {total > highScore ? total : highScore}</h1>
       </div>
       <div className="row">
-        <button onClick={() => window.location.reload()} className="btn-wide btn-accent">
+        <button
+          onClick={() => {
+            updateHighScore(total);
+            window.location.reload();
+          }}
+          className="btn-wide btn-accent"
+        >
           Play again!
         </button>
       </div>
